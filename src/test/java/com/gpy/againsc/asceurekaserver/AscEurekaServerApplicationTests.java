1adc7c4bc12b85e58f6f82e2275d8cf7cef2c07f
guopengying <1315977102@qq.com>
2020-07-23 16:22:40 +0800

    init eureka server

---
+++ src/test/java/com/gpy/againsc/asceurekaserver/AscEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gpy.againsc.asceurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AscEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
